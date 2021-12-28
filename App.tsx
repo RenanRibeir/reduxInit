f5b920ce8c1c6f5bfc8dc388c61e05f9157d48e0
renanribeir <renanribeirow19@gmail.com>
2021-12-28 11:27:53 -0300

    feature: implement docment redux exemple

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {Counter} from './src/counter/Counter';
+import store from './src/store';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text />
-    </SafeAreaView>
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
 };
 
